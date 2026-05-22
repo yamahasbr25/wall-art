f4996eb430609695a509277d8da81f2d32658df4
yamahasbr25 <yamahasbr25@gmail.com>
2026-05-22 09:20:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 const linkUrl = `detail.html?q=${encodeURIComponent(keywordForUrl)}`; 
 
                 const queryImage = keyword + " wall art poster";
-                const imageUrl = `https://tse1.mm.bing.net/th?q=${encodeURIComponent(queryImage)}&w=500&pid=1.7`;
+                const imageUrl = `https://tse1.mm.bing.net/th?q=${encodeURIComponent(queryImage)}&w=400&h=600&c=7&rs=1&p=0&dpr=1.5&pid=1.7`;
                 
                 const newTitle = generateSeoTitle(keyword);
                 const cardHTML = `<article class="content-card"><a href="${linkUrl}"><img src="${imageUrl}" alt="${newTitle}" loading="lazy"><div class="content-card-body"><h3>${newTitle}</h3></div></a></article>`;
